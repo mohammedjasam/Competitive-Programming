bb46e9aab4c7976f7684576e794d9709995ea91d
Mohammed Jasam <mnqnd@mst.edu>
2017-03-07 20:03:03 -0600

    Unable to solve SevenWonders 8/27 test cases work, no idea why others dont

---
+++ SevenWonders.py
@@ -0,0 +1,29 @@
+A = input()
+if len(A)>50:
+    pass
+else:
+    T,C,G=0,0,0
+    # pts=0
+
+    for i in range(len(A)):
+        if A[i]=='T':
+            T+=1
+        elif A[i]=='C':
+            C+=1
+        elif A[i]=='G':
+            G+=1
+    print(T,C,G)
+    if T <= C | T <= G:
+        smallest = T
+    elif C <= G:
+        smallest = C
+    else:
+        smallest = G
+
+    T = T*T
+    C = C*C
+    G = G*G
+
+    print(smallest)
+    pts = T+C+G+(7*smallest)
+    print(pts)
