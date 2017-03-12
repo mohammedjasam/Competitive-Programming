605982d5a6b0a63258254f6f1efa3b62f4a48cd2
Mohammed Jasam <mnqnd@mst.edu>
2017-03-11 23:24:12 -0600

    Solved ABC

---
+++ ABC.py
@@ -0,0 +1,14 @@
+s=[int(x) for x in input().split()]
+s.sort()
+
+op=input()
+st=""
+for i in range(len(s)):
+    if op[i]=='A':
+        st+=str(s[0])+" "
+    elif op[i]=='B':
+        st+=str(s[1])+" "
+    elif op[i]=='C':
+        st+=str(s[2])+ " "
+
+print(st[:-1])
