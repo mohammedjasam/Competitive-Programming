bcfef38e6b7f29ae496b676d84fb7eb84a6239a0
Mohammed Jasam <mnqnd@mst.edu>
2017-03-11 20:30:40 -0600

    Easy way is using lists!

--- BookingARoom.py
+++ BookingARoom.py
@@ -3,10 +3,11 @@ total,booked = map(int,input().split())
 log={}
 for i in range(total):
     log[i+1]=0
+# print(log)
 for i in range(booked):
     room_no=int(input())
     log[room_no]=1
-
+# print(log)
 if total>booked:
     for k, v in log.items():
         if log[k]==0:
