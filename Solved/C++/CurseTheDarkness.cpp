71b63d59be0a54288eaeb8deb3ceead56333c8fd
Mohammed Jasam <mnqnd@mst.edu>
2017-04-10 01:31:52 -0500

    First Solved C++ Problem

---
+++ Solved/C++/CurseTheDarkness.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+double square(double a){
+	return a * a;
+}
+
+int main(void)
+{
+	int tc,n,canLight;
+	double ux,uy,cx,cy;
+
+	cin>>tc;
+
+	while(tc--)
+	{
+		cin>>ux>>uy;
+		cin>>n;
+		canLight=0;
+		for (int j = 0; j < n; ++j)
+		{
+			cin>>cx>>cy;
+
+			double d=sqrt(square(ux-cx) + square(uy-cy));
+			// cout<<d;
+			if (d<=8.0000000)
+			{
+				canLight++;
+			}
+		}
+
+		if (canLight!=0)
+		{
+			cout<<"light a candle\n";
+		}
+		else
+		{
+			cout<<"curse the darkness\n";
+		}
+	}
+	// cout<<"Terminated!";
+	return 0;
+}
