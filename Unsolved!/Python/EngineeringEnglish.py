2cb505fca1f9756305fe03f493838ef96bcd0095
Mohammed Jasam <mnqnd@mst.edu>
2017-11-24 23:49:53 -0600

    Modified the files

--- Unsolved!/Python/EngineeringEnglish.py
+++ Unsolved!/Python/EngineeringEnglish.py
@@ -1,8 +1,32 @@
-import sys
-for line in sys.stdin:
-    wordlist=line.split()
-    wordfreq = []
-    for w in wordlist:
-        wordfreq.append((wordlist.count(w),w))
-    print("List\n" + str(wordlist) + "\n")
-    print("Frequencies\n" + str(wordfreq) + "\n")
+movies = ["jasam", "pooja", "weer", "shalini", "ross"]
+
+import random as rn
+
+def run():
+    element = movies[rn.randint(0,4)]
+    print(element)
+    correct = ""
+    for i in element:
+        correct += "_"
+
+
+    for i in range(10):
+        guess = input()
+
+        indices = []
+        for i in range(len(element)):
+            if guess == element[i]:
+                indices.append(i)
+
+        for i in indices:
+            a = list(correct)
+            a[i] = guess[0]
+            correct = "".join(a)
+
+        if correct == element:
+            print(correct)
+            break
+
+        print(correct)
+
+run()
